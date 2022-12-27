b6d21c015588c727c52f2e6bd47206ec74f1f557
Inrixia <Inrixia@gmail.com>
2022-12-28 05:04:12 +1300

    Fixed realTime simulation

--- baro.ts
+++ baro.ts
@@ -43,49 +43,52 @@ const reactorControllerTick = (): [setTurbine: number, setFission: number] => {
 	return [turbineRate, fissionRate];
 };
 
-const simSeconds = 4 * 60 + 22;
-const realTime = false;
+const simSeconds = 4 * 60;
+const realTime = true;
 
 let tick = 0;
-while (tick <= tickRate * simSeconds) {
-	tick++;
-	const [turbineRate, fissionRate] = reactorControllerTick();
-	reactor.tick();
+const maxTicks = tickRate * simSeconds;
+(async () => {
+	while (realTime || tick <= maxTicks) {
+		tick++;
+		const [turbineRate, fissionRate] = reactorControllerTick();
+		reactor.tick();
 
-	const melted = reactor.GetMelted();
+		const melted = reactor.GetMelted();
 
-	if (realTime || (!realTime && tick >= tickRate * simSeconds) || melted) {
-		console.clear();
-		console.log(`Power: ${reactor.GetPower().toFixed(2)} kW`);
-		console.log(`Fuel: ${reactor.GetFuel()}`);
-		console.log(`Fuel Percentage Left: ${reactor.GetFuelPercentageLeft().toFixed(2)}%`);
-		console.log(`Temperature: ${(reactor.GetTemperature() / 100).toFixed(2)}%`);
-		console.log(`Load: ${reactor.GetLoad()} kW`);
+		if (realTime || (!realTime && tick >= tickRate * simSeconds) || melted) {
+			console.clear();
+			console.log(`Power: ${reactor.GetPower().toFixed(2)} kW`);
+			console.log(`Fuel: ${reactor.GetFuel()}`);
+			console.log(`Fuel Percentage Left: ${reactor.GetFuelPercentageLeft().toFixed(2)}%`);
+			console.log(`Temperature: ${(reactor.GetTemperature() / 100).toFixed(2)}%`);
+			console.log(`Load: ${reactor.GetLoad()} kW`);
 
-		console.log();
-		console.log(`[Turbine] - Real: ${reactor.GetHiddenTurbineOutput().toFixed(2)}, Set: ${turbineRate.toFixed(2)}`);
-		console.log(`[Fission] - Real: ${reactor.GetHiddenFissionRate().toFixed(2)}, Set: ${fissionRate.toFixed(2)}`);
-		console.log();
+			console.log();
+			console.log(`[Turbine] - Real: ${reactor.GetHiddenTurbineOutput().toFixed(2)}, Set: ${turbineRate.toFixed(2)}`);
+			console.log(`[Fission] - Real: ${reactor.GetHiddenFissionRate().toFixed(2)}, Set: ${fissionRate.toFixed(2)}`);
+			console.log();
 
-		console.log();
-		console.log(
-			`Rods:${reactor.GetRods().map((rod) => {
-				if (rod === null) return ` null`;
-				return ` ${((rod.durability / rod.maxDurability) * 100).toFixed(2)}%`;
-			})}`
-		);
-		console.log(`Temperature Critical: ${reactor.GetTemperatureCritical()}`);
-		console.log(`Temperature Hot: ${reactor.GetTemperatureHot()}`);
-		console.log();
-		console.log(`On Fire: ${reactor.GetOnFire() / tickRate}s`);
-		console.log(`Melted: ${reactor.GetMelted()}`);
-		console.log(`Health: ${reactor.GetHealth()}%`);
-		console.log();
+			console.log();
+			console.log(
+				`Rods:${reactor.GetRods().map((rod) => {
+					if (rod === null) return ` null`;
+					return ` ${((rod.durability / rod.maxDurability) * 100).toFixed(2)}%`;
+				})}`
+			);
+			console.log(`Temperature Critical: ${reactor.GetTemperatureCritical()}`);
+			console.log(`Temperature Hot: ${reactor.GetTemperatureHot()}`);
+			console.log();
+			console.log(`On Fire: ${reactor.GetOnFire() / tickRate}s`);
+			console.log(`Melted: ${reactor.GetMelted()}`);
+			console.log(`Health: ${reactor.GetHealth()}%`);
+			console.log();
 
-		console.log(`Tick: ${tick}, Sec: ${tick / tickRate}`);
+			console.log(`Tick: ${tick}, Sec: ${tick / tickRate}`);
+		}
+
+		if (melted) break;
+
+		if (realTime) await sleep(1000 / tickRate);
 	}
-
-	if (melted) break;
-
-	// sleep(1000 / tickRate).then(mainLoop);
-}
+})();
